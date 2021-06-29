8d25a854bae09341d7cdbd5814e72ac2f2a830b6
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-29 16:34:44 -0300

    instaciando websocket server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   return res.json({
