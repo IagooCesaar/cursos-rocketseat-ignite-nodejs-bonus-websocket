34e71cb0a701ce2e994c9822561d478667f9ad30
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-30 10:20:39 -0300

    CreateUserService

---
+++ src/services/CreateUserServices.ts
@@ -0,0 +1,44 @@
+import { injectable } from "tsyringe";
+import { User } from "../schemas/User"
+
+interface CreateUserDTO {
+  socket_id: string;
+  avatar: string;
+  name: string;
+  email: string;
+}
+
+@injectable()
+class CreateUserUseService {
+
+  async execute({
+    avatar,
+    email,
+    name,
+    socket_id
+  }: CreateUserDTO) {
+    const userAlreadyExists = await User.findOne({
+      email,
+    }).exec();
+
+    if(userAlreadyExists) {
+      const user = await User.findOneAndUpdate({
+        _id: userAlreadyExists._id,
+      }, {
+        $set: {
+          socket_id, avatar, name
+        }
+      })
+      return user;
+    }
+    const user = await User.create({
+      email,
+      name,
+      avatar,
+      socket_id,
+    })
+    return user;
+  }
+}
+
+export { CreateUserUseService }
