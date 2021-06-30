e5c7654f52031a93afd7ddab2542b02351ce0c2b
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-30 13:55:09 -0300

    preparando evento para transmitir todos usuários

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,14 +1,17 @@
 import { container } from 'tsyringe'
 import { io } from '../http'
-import { CreateUserService } from '../services/CreateUserServices'
+import { CreateUserService } from '../services/CreateUserService'
 
 // socket.emit() -> envio direcionado a um grupo de conexões
-// socket.broadcast.emit() -> envia para todos os usuário, exceto para o proprio remetente
+// socket.broadcast.emit() -> envia para todos os usuário, exceto para o próprio remetente
 // io.emit() -> broadcast para todos usuários
 // socket.on() -> evento para "ouvir" 
+
 io.on("connect", socket => {
   // recuperando dados da conexão (evento "on connection")
+
   socket.on("start", async (data) => {
+    // "start" é emitido pelo frontend
     const { email, avatar, name } = data;
     const socket_id = socket.id;
     const createUserService = container.resolve(CreateUserService);
@@ -18,6 +21,13 @@ io.on("connect", socket => {
       name,
       socket_id,
     })
-    socket.broadcast.emit("new_users", user)
+    //Enviando usuário para todos os outros conectados
+    socket.broadcast.emit("new_users", user);
+
+
+  })
+
+  socket.on("get_users", () => {
+
   })
 })
