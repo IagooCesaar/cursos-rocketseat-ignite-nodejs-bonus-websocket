5a0087660ecbeaf952cb056acab72739f6548680
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-30 09:59:15 -0300

    conectando ao mongodb

--- src/http.ts
+++ src/http.ts
@@ -2,10 +2,16 @@ import express from 'express'
 import path from 'path'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
+import mongoose from 'mongoose'
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+
+mongoose.connect("mongodb://localhost/rocketsocket", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 
 io.on("connection", (socket) => {
   // Aqui o serviço é inicializado
