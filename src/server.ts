315bf23cddc2b8aa2f20243fa53277c903dbfbd3
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-30 07:22:23 -0300

    separando responsabilidades

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,4 @@
-import { createServer } from 'http'
-import path from 'path'
-import express from 'express'
-import { Server } from 'socket.io'
-
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
-
-io.on("connection", (socket) => {
-  console.log("soket", socket.id)
-})
-
-app.use(express.static(path.join(__dirname,"..", "public")))
-
-app.get('/', (req, res) => {
-  return res.json({
-    message: "Hello Websocket"
-  })
-})
+import { server } from './http'
 
 server.listen(3000, () => {
   console.log("App is running on port 3000🚀")
